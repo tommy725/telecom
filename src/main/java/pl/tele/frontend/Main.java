71a55c2f082d0075803d8a93c40bf955c1aa7631
Tommy <afteryou725@gmail.com>
2022-03-27 20:48:33 +0200

    Full docs to ex.2 (Xmodem)

--- src/main/java/pl/tele/frontend/Main.java
+++ src/main/java/pl/tele/frontend/Main.java
@@ -14,39 +14,39 @@ public class Main {
         String[] portsList = PortManager.getPortsNameList();
         System.out.println("Wybierz port:");
         for (String port : portsList) {
-            System.out.println(port);
+            System.out.println(port); //Display port list
         }
         System.out.print("Wybór: ");
-        int port = (new Scanner(System.in)).nextInt();
+        int port = (new Scanner(System.in)).nextInt(); //Choose port
 
         System.out.print("Nadajnik / Odbiornik: ");
-        String no = (new Scanner(System.in)).nextLine();
+        String no = (new Scanner(System.in)).nextLine(); //Choose if sender of receiver
         switch (no) {
-            case "Odbiornik" -> {
-                System.out.print("Sum / CRC: ");
+            case "Odbiornik" -> { //if receiver
+                System.out.print("Sum / CRC: "); //check checksum version (crc or checksums)
                 String cs = (new Scanner(System.in)).nextLine();
-                try (ReceiverPort receiverPort = (ReceiverPort) PortManager.inicializePort(port, true)) {
+                try (ReceiverPort receiverPort = (ReceiverPort) PortManager.inicializePort(port, true)) { //inicialize port
                     System.out.println("Komunikacja rozpoczęta jako: " + no);
                     switch (cs) {
-                        case "Sum" -> receiverPort.initializeSumConnection();
-                        case "CRC" -> receiverPort.initializeCRCConnection();
+                        case "Sum" -> receiverPort.initializeSumConnection(); //inicialize checksum version connection
+                        case "CRC" -> receiverPort.initializeCRCConnection(); //inicialize crc version connection
                         default -> System.out.println("Wybrano nieprawidłową opcję");
                     }
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
             }
-            case "Nadajnik" -> {
+            case "Nadajnik" -> { //if sender
                 byte[] fileBytes = null;
-                System.out.print("Podaj ścieżkę do pliku: ");
+                System.out.print("Podaj ścieżkę do pliku: "); //Import file to send
                 Path path = Paths.get((new Scanner(System.in)).nextLine());
                 if (!Files.exists(path)) {
-                    System.out.println("Plik nie znaleziony!");
+                    System.out.println("Plik nie znaleziony!"); //If file not exists stop
                     return;
                 }
-                try (SenderPort senderPort = (SenderPort) PortManager.inicializePort(port, false)) {
-                    fileBytes = Files.readAllBytes(path);
-                    senderPort.setFileBytes(fileBytes);
+                try (SenderPort senderPort = (SenderPort) PortManager.inicializePort(port, false)) { //inicialize port
+                    fileBytes = Files.readAllBytes(path); //read all bytes of the file
+                    senderPort.setFileBytes(fileBytes); //send bytes to sender port object
                     System.out.println("Komunikacja rozpoczęta jako: " + no);
                     while (true) {
                         (new Scanner(System.in)).nextLine();
@@ -55,7 +55,7 @@ public class Main {
                     System.out.println(e.getMessage());
                 }
             }
-            default -> System.out.println("Wybrano nieprawidłową opcję");
+            default -> System.out.println("Wybrano nieprawidłową opcję"); //incorrect option
         }
     }
 }
--- src/main/java/pl/tele/frontend/SenderSerialPortListener.java
+++ src/main/java/pl/tele/frontend/SenderSerialPortListener.java
@@ -14,42 +14,46 @@ public class SenderSerialPortListener implements SerialPortDataListener {
         return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
     }
 
+    /**
+     * Sender port listener controlling transmission of the file
+     * @param serialPortEvent port event
+     */
     @Override
     public void serialEvent(SerialPortEvent serialPortEvent) {
         byte[] receivedData = serialPortEvent.getReceivedData();
         SenderPort sp = (SenderPort) PortManager.getPort(serialPortEvent.getSerialPort().getSystemPortName());
-        if (sp.getBlockNumber() + 1 >= sp.getBlockToSend() && (sp.isConnected() || sp.isEndingTransmission())) {
+        if (sp.getBlockNumber() + 1 >= sp.getBlockToSend() && (sp.isConnected() || sp.isEndingTransmission())) { //is ending transmission/connection
             if (!sp.isConnected() && receivedData.length == 1 && receivedData[0] == ACK) {
-                System.out.println("ZAKONCZONO POLACZENIE.");
+                System.out.println("ZAKONCZONO POLACZENIE."); //after sending ETB we got ACK so we end connection and end program.
                 System.exit(0);
             }
             if (sp.isEndingTransmission() && receivedData.length == 1 && (receivedData[0] == ACK || !sp.isConnected())) {
-                System.out.println("ZAKONCZONO POLACZENIE. WYSYLANIE ETB");
+                System.out.println("ZAKONCZONO POLACZENIE. WYSYLANIE ETB"); //after finishing transfer we want to end connection (send ETB)
                 sp.setConnected(false);
                 sp.send(ETB);
             } else {
-                System.out.println("ZAKONCZONO PRZESYLANIE PLIKU. WYSYLANIE EOT");
+                System.out.println("ZAKONCZONO PRZESYLANIE PLIKU. WYSYLANIE EOT"); //all blocks of file are transmitted so we want to end transmission (send EOT)
                 sp.endTransmission();
                 sp.send(EOT);
             }
             return;
         }
         if (sp.isConnected() && receivedData.length == 1 && receivedData[0] == NAK) {
-            System.out.println("OTRZYMANO NACK. WYSYLANIE PONOWNIE TEGO SAMEGO BLOKU DANYCH");
+            System.out.println("OTRZYMANO NACK. WYSYLANIE PONOWNIE TEGO SAMEGO BLOKU DANYCH"); //block received by the receiver is incorrect and block has to be repeated.
             sp.send(sp.getDataBlock(false));
         }
         if (sp.isConnected() && (receivedData.length == 1) && (receivedData[0] == ACK)) {
-            System.out.println("OTRZYMANO ACK. WYSYLANIE KOLEJNEGO BLOKU DANYCH");
+            System.out.println("OTRZYMANO ACK. WYSYLANIE KOLEJNEGO BLOKU DANYCH"); //block received by the receiver is correct so we send next block
             sp.send(sp.getDataBlock(true));
         }
-        if (!sp.isConnected() && receivedData.length == 1 && (receivedData[0] == NAK || receivedData[0] == C)) {
-            sp.initializeConnection(receivedData[0]);
-            switch (receivedData[0]) {
+        if (!sp.isConnected() && receivedData.length == 1 && (receivedData[0] == NAK || receivedData[0] == C)) { //transmission is not yet inicialized.
+            sp.initializeConnection(receivedData[0]);                //inicialize connection
+            switch (receivedData[0]) {                               //inicialize CRC/checksum transmission depending on the argument received from the user
                 case NAK -> System.out.println("OTRZYMANO NACK (" + receivedData[0] + "). INICJACJA POLACZENIA Z SUMA KONTROLNA");
                 case C -> System.out.println("OTRZYMANO C (" + receivedData[0] + "). INICJACJA POLACZENIA Z CRC");
             }
             System.out.println("WYSYLANIE PIERWSZEGO BLOKU DANYCH");
-            sp.send(sp.getDataBlock(false));
+            sp.send(sp.getDataBlock(false)); //send first file data block
         }
     }
 }
